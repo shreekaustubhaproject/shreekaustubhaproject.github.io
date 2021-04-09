ebacf4cd440ac6f92ec618f1aa8d70bbdb3e26c1
shreekaustubhaproject <80771222+shreekaustubhaproject@users.noreply.github.com>
2021-04-09 21:32:13 +0530

    Update skp.js

--- skp.js
+++ skp.js
@@ -7,10 +7,6 @@ if ('serviceWorker' in navigator) {
 }
 
 
- var isChrome = /Chrome/.test(navigator.userAgent) && /Google Inc/.test(navigator.vendor);
-  if (!isChrome){
-      $('#iframeAudio').remove()
-  }
-  else {
-      $('#playAudio').remove() // just to make sure that it will not have 2x audio in the background 
-  }
+    window.onload=function(){
+      document.getElementById("my_audio").play();
+    }

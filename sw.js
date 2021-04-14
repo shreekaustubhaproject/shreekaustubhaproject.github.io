2d297f9b29ff0603661b7f106e1915b8561fccbd
shreekaustubhaproject <80771222+shreekaustubhaproject@users.noreply.github.com>
2021-04-14 16:59:36 +0530

    Update sw.js

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-var CACHE_STATIC_NAME = 'static-v7';
-var CACHE_DYNAMIC_NAME = 'dynamic-v7';
+var CACHE_STATIC_NAME = 'static-v8';
+var CACHE_DYNAMIC_NAME = 'dynamic-v8';
 
 self.addEventListener('install', function(event) {
   console.log('[Service Worker] Installing Service Worker ...', event);
@@ -56,24 +56,26 @@ self.addEventListener('activate', function(event) {
 
 self.addEventListener('fetch', function(event) {
   event.respondWith(
-	caches.match(event.request)
-	.then(function(response){
-		if (response){
-			return response;
-		} else {
-			return fetch(event.request)
-			.then(function (res) {
-				return caches.open(CACHE_DYNAMIC_NAME)
-				.then(function(cache){
-					cache.put(event.request.url, res.clone());
-					return res;
-				})
-			})
-			.catch(function(err) {
-			
-			});
-		}
-	})
-  
+    caches.match(event.request)
+      .then(function(response) {
+        if (response) {
+          return response;
+        } else {
+          return fetch(event.request)
+            .then(function(res) {
+              return caches.open(CACHE_DYNAMIC_NAME)
+                .then(function(cache) {
+                  cache.put(event.request.url, res.clone());
+                  return res;
+                })
+            })
+            .catch(function(err) {
+              return caches.open(CACHE_STATIC_NAME)
+                .then(function(cache) {
+                  return cache.match('/offline.html');
+                });
+            });
+        }
+      })
   );
 });
